4748d427181e5bc605b26f11ad5f3f897d6990b4
luisalomia <luisfidel44@gmail.com>
2022-05-11 16:09:37 -0500

    FLAVportfolio v 1.0.0

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+const btnInicioUne = document.getElementById("btnInicioUne");
+const inicioActive = document.getElementById("inicioActive");
+const btnProjectUne = document.getElementById("btnProjectUne");
+const projectActive = document.getElementById("projectActive");
+const btnAboutUne = document.getElementById("btnAboutUne");
+const aboutActive = document.getElementById("aboutActive");
+const btnMenu = document.getElementById("btnMenu");
+const menuHidden = document.getElementById("menuHidden");
+const btnInicio = document.getElementById("btnInicio");
+const inicioActiveHamb = document.getElementById("inicioActiveHamb");
+const btnProject = document.getElementById("btnProject");
+const projectActiveHamb = document.getElementById("projectActiveHamb");
+const btnAbout = document.getElementById("btnAbout");
+const aboutActiveHamb = document.getElementById("aboutActiveHamb");
+const btnContactHeader = document.getElementById("btnContactHeader");
+const hiddenBtnHeader = document.getElementById("hiddenBtnHeader");
+const btnContactAbout = document.getElementById("btnContactAbout");
+const hiddenBtnAbout = document.getElementById("hiddenBtnAbout");
+
+/* Navegacion */
+
+btnInicioUne.addEventListener("click", () => {
+  inicioActive.classList.add("active");
+  projectActive.classList.remove("active");
+  aboutActive.classList.remove("active");
+});
+
+btnProjectUne.addEventListener("click", () => {
+  projectActive.classList.add("active");
+  inicioActive.classList.remove("active");
+  aboutActive.classList.remove("active");
+});
+
+btnAboutUne.addEventListener("click", () => {
+  aboutActive.classList.add("active");
+  projectActive.classList.remove("active");
+  inicioActive.classList.remove("active");
+});
+
+/* Menu hamburguesa */
+
+btnMenu.addEventListener("click", () => {
+  menuHidden.classList.toggle("hidden");
+});
+
+btnInicio.addEventListener("click", () => {
+  inicioActiveHamb.classList.add("active");
+  projectActiveHamb.classList.remove("active");
+  aboutActiveHamb.classList.remove("active");
+  menuHidden.classList.toggle("hidden");
+});
+
+btnProject.addEventListener("click", () => {
+  projectActiveHamb.classList.add("active");
+  inicioActiveHamb.classList.remove("active");
+  aboutActiveHamb.classList.remove("active");
+  menuHidden.classList.toggle("hidden");
+});
+
+btnAbout.addEventListener("click", () => {
+  aboutActiveHamb.classList.add("active");
+  projectActiveHamb.classList.remove("active");
+  inicioActiveHamb.classList.remove("active");
+  menuHidden.classList.toggle("hidden");
+});
+
+/* Botones de contacto */
+
+btnContactHeader.addEventListener("click", () => {
+  hiddenBtnHeader.classList.toggle("hidden");
+});
+
+btnContactAbout.addEventListener("click", () => {
+  hiddenBtnAbout.classList.toggle("hidden");
+});
